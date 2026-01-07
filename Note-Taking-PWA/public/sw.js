822267b28d667352d6595d7d49759f90d016ece2
Ilekuba Oluebube Collins <cilekuba.2502996@stu.cu.edu.ng>
2026-01-08 00:32:39 +0100

    The final change

--- Note-Taking-PWA/public/sw.js
+++ Note-Taking-PWA/public/sw.js
@@ -1,12 +1,12 @@
 const CACHE_NAME = 'offline-notes-v3';
 const STATIC_ASSETS = [
-    './',
-    './index.html',
-    './src/style.css',
-    './src/app.js',
-    './src/idb.js',
-    './manifest.json',
-    './public/Note.png'
+    '/',
+    '/index.html',
+    '/style.css',
+    '/src/app.js',
+    '/src/idb.js',
+    '/manifest.json',
+    '/public/Note.png'
 ];
 
 self.addEventListener('install', (event) => {
